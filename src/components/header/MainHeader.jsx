b7d3fa750bfa978212be2aa57592604e2316ad64
Azis <you@example.com>
2024-01-09 15:01:10 +0600

    feat: header contine products menu

--- src/components/header/MainHeader.jsx
+++ src/components/header/MainHeader.jsx
@@ -6,49 +6,53 @@ import like from '../../assets/icon/like.png'
 import shop from '../../assets/icon/shop.png'
 import search from '../../assets/icon/serch.png'
 import personal from '../../assets/icon/personal.png'
+import MenuProducts from './MenuProducts'
 
 const MainHeader = () => (
-   <header className={classes.headerContent}>
-      <div className={classes.container}>
-         <div className={classes.burgerMenu}>
-            <span className={classes.burgerItem} />
-            <span className={classes.burgerItem2} />
-            <span className={classes.burgerItem2} />
-            <span className={classes.burgerItem} />
-         </div>
-
-         <ul className={classes.ulContainer}>
-            <li>Товары</li>
-            <li>Услуги</li>
-            <li>Компания</li>
-         </ul>
-
-         <div>
-            <img className={classes.headerLogo} src={logo} alt="logo" />
-         </div>
-
-         <ul>
-            <li className={classes.headerIcon}>
-               <img src={search} alt="search" />
-            </li>
-            <li className={classes.personalIcon}>
-               <img src={personal} alt="" />
-            </li>
-
-            <div className={classes.divContainerUl}>
-               <li>Личный кабинет</li>
-
-               <li className={classes.headerIcon}>
-                  <img src={like} alt="" />
-               </li>
-
-               <li className={classes.headerIcon}>
-                  <img src={shop} alt="" />
-               </li>
+   <>
+      <header className={classes.headerContent}>
+         <div className={classes.container}>
+            <div className={classes.burgerMenu}>
+               <span className={classes.burgerItem} />
+               <span className={classes.burgerItem2} />
+               <span className={classes.burgerItem2} />
+               <span className={classes.burgerItem} />
             </div>
-         </ul>
-      </div>
-   </header>
+
+            <ul className={classes.ulContainer}>
+               <li>Товары</li>
+               <li>Услуги</li>
+               <li>Компания</li>
+            </ul>
+
+            <div>
+               <img className={classes.headerLogo} src={logo} alt="logo" />
+            </div>
+
+            <ul>
+               <li className={classes.headerIcon}>
+                  <img src={search} alt="search" />
+               </li>
+               <li className={classes.personalIcon}>
+                  <img src={personal} alt="" />
+               </li>
+
+               <div className={classes.divContainerUl}>
+                  <li>Личный кабинет</li>
+
+                  <li className={classes.headerIcon}>
+                     <img src={like} alt="" />
+                  </li>
+
+                  <li className={classes.headerIcon}>
+                     <img src={shop} alt="" />
+                  </li>
+               </div>
+            </ul>
+         </div>
+      </header>
+      <MenuProducts />
+   </>
 )
 
 export default MainHeader
