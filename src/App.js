65451f94bcdedfe6797a4275d8066cc1d4b95ad7
KalykovAJ <aj.kalyk@gmail.com>
2024-01-10 15:12:45 +0600

    feat: new components PopTires, ProductItem, Button

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import React from 'react'
 import MainSlider from './components/main-section/MainSlider'
 import Layout from './layout/Layout'
+import PopTires from './components/main-section/PopTires'
 
 const App = () => (
    <Layout>
       <MainSlider />
+      <PopTires />
    </Layout>
 )
 
