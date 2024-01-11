aaf37ad9e23fe1a96e51a760581754b21d15ae42
KalykovAJ <aj.kalyk@gmail.com>
2024-01-11 15:55:58 +0600

    feat: new components Partners and Ticker

--- src/App.js
+++ src/App.js
@@ -2,11 +2,15 @@ import React from 'react'
 import MainSlider from './components/main-section/MainSlider'
 import Layout from './layout/Layout'
 import PopTires from './components/main-section/PopTires'
+import Partners from './components/main-section/Partners'
+import Ticker from './components/main-section/Ticker'
 
 const App = () => (
    <Layout>
       <MainSlider />
       <PopTires />
+      <Ticker />
+      <Partners />
    </Layout>
 )
 
