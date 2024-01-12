c85e83a1a84b9d6e4c55c7e57aec03eb13530720
Azis <you@example.com>
2024-01-12 15:27:09 +0600

    fix:  ProuctItem component location

--- src/App.js
+++ src/App.js
@@ -1,6 +1,11 @@
 import React from 'react'
 import HomePage from './components/home/HomePage'
+import Layout from './layout/Layout'
 
-const App = () => <HomePage />
+const App = () => (
+   <Layout>
+      <HomePage />
+   </Layout>
+)
 
 export default App
--- src/components/main-section/PopTires.jsx
+++ src/components/main-section/PopTires.jsx
@@ -1,7 +1,7 @@
 import React from 'react'
 import classes from './PopTires.module.css'
-import ProductItem from './ProductItem'
 import Button from '../ui/Button'
+import ProductItem from '../ui/Card/ProductItem'
 
 const PopTires = () => {
    return (
