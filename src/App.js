65ba5b4df1cb04556e7c25d21e46b35f28bdf20d
indira016 <indira.sharshebaevaa.alt@gmail.com>
2024-01-12 15:10:46 +0600

    section-sale

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import React from 'react'
-import MainSlider from './components/main-section/MainSlider'
+// import MainSlider from './components/main-section/MainSlider'
 import Layout from './layout/Layout'
+import Sale from './components/sale-section/Sale'
 
 const App = () => (
    <Layout>
-      <MainSlider />
+      {/* <MainSlider /> */}
+      <Sale />
    </Layout>
 )
 
