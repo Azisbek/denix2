5afcb89ff42849faf6b2a94724101453e05d76c0
Azis <you@example.com>
2024-01-15 14:22:37 +0600

    fix: option select

---
+++ src/components/main-section/Selection.jsx
@@ -0,0 +1,18 @@
+import React from 'react'
+import classes from './Selection.module.css'
+
+const Selection = () => {
+   return (
+      <div className={classes.containerTitle}>
+         <div className={classes.optionsTitle}>
+            <h1>
+               Подберём
+               <span className={classes.solid}>шины по марке автомобиля </span>
+               или <span className={classes.dashed}>параметрам товара</span>
+            </h1>
+         </div>
+      </div>
+   )
+}
+
+export default Selection
