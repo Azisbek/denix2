d6b412efb9de039e73ddb42cb7b6b9637be167b9
Azis <you@example.com>
2024-01-15 17:06:00 +0600

    feat: components CatalogSideBar

--- src/components/catalog/CatalogPage.jsx
+++ src/components/catalog/CatalogPage.jsx
@@ -1,12 +1,15 @@
 import React from 'react'
 import CatalodSideBar from './sidebar/CatalodSideBar'
 import ProductList from './ProductList'
+import classes from './CatalogPage.module.css'
 
 const CatalogPage = () => {
    return (
-      <div>
-         <ProductList />
-         <CatalodSideBar />
+      <div className={classes.container}>
+         <div className={classes.block}>
+            <CatalodSideBar />
+            <ProductList />
+         </div>
       </div>
    )
 }
