f5c1d14652ae7aed5f17eac9547753815ce5ba7a
Azis <you@example.com>
2024-01-16 10:19:30 +0600

    fix: image path

--- src/components/catalog/CatalogBanner.jsx
+++ src/components/catalog/CatalogBanner.jsx
@@ -1,14 +1,18 @@
 import React from 'react'
 import classes from './CatalogBanner.module.css'
+import Button from '../ui/Button'
 
 const CatalogBanner = () => {
    return (
       <div className={classes.banner}>
-         <h1>Поможем сделать правильный выбор</h1>
-         <p>
-            Оставьте заявку и наши менеджеры подберут для вашего автомобиля
-            требуемый товар
-         </p>
+         <div className={classes.content}>
+            <h1>Поможем сделать правильный выбор</h1>
+            <p>
+               Оставьте заявку и наши менеджеры подберут для вашего автомобиля
+               требуемый товар
+            </p>
+            <Button>Оставить заявку</Button>
+         </div>
       </div>
    )
 }
