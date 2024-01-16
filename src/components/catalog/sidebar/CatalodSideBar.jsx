afc2b7bda2c6a3e166063bcd9cf07c703c63ef1b
Azis <you@example.com>
2024-01-16 17:02:32 +0600

    fix: catalog sidebar

--- src/components/catalog/sidebar/CatalodSideBar.jsx
+++ src/components/catalog/sidebar/CatalodSideBar.jsx
@@ -3,188 +3,190 @@ import classes from './CatalogSideBar.module.css'
 
 const CatalodSideBar = () => {
    return (
-      <form className={classes.tieres}>
-         <h1>Шины в Бишкеке</h1>
-         <p className={classes.tieresTitle}>
-            Ширина <span className={classes.tieresSpan}>?</span>
-         </p>
-         <select name="Все" id="">
-            <option value="Все">Все</option>
-            <option value="Все">Все</option>
-            <option value="Все">Все</option>
-         </select>
-         <p className={classes.tieresTitle}>
-            Профиль <span className={classes.tieresSpan}>?</span>
-         </p>
-         <select name="Все" id="">
-            <option value="Все">Все</option>
-            <option value="Все">Все</option>
-            <option value="Все">Все</option>
-         </select>
-         <p className={classes.tieresTitle}>
-            Диаметр <span className={classes.tieresSpan}>?</span>
-         </p>
-         <select name="Все" id="">
-            <option value="Все">Все</option>
-            <option value="Все">Все</option>
-            <option value="Все">Все</option>
-         </select>
-         <p className={classes.tieresTitle}>Цена</p>
-         <div className={classes.price}>
-            <div className={classes.priceitemInput}>
-               <input type="text" placeholder="от 1234" />
-               <input type="text" placeholder="до 50234" />
-            </div>
-            <input type="range" name="" id="" />
-         </div>
-         <div className={classes.tireType}>
-            <p>Тип шин</p>
-            <label>
-               <input type="checkbox" name="option1" /> Легковое
-            </label>
-
-            <label>
-               <input type="checkbox" name="option2" />
-               Внедорожник (SUV)
-            </label>
-
-            <label>
-               <input type="checkbox" name="option3" /> Легкогрузовой (LTR)
-            </label>
-            <p>Сезонность</p>
-            <label>
-               <input type="checkbox" name="option1" /> Зима
-            </label>
-
-            <label>
-               <input type="checkbox" name="option2" /> Лето
-            </label>
-
-            <label>
-               <input type="checkbox" name="option3" /> Всесезонное
-            </label>
-            <p>Состояние</p>
-            <label>
-               <input type="checkbox" name="option1" /> Только новые
-            </label>
-         </div>
-         <div className={classes.inputContainer}>
-            <p>Производитель</p>
-            <span className={classes.icon}>🔍</span>
-            <input
-               type="text"
-               className={classes.inputText}
-               placeholder="Введите название"
-            />
-         </div>
-         <div>
-            <label>
-               <input type="checkbox" name="option1" /> Nokian
-            </label>
-            <label>
-               <input type="checkbox" name="option1" /> Pirelli
-            </label>
-            <label>
-               <input type="checkbox" name="option1" /> Hankook
-            </label>
-            <label>
-               <input type="checkbox" name="option1" /> Michelin
-            </label>
-            <label>
-               <input type="checkbox" name="option1" /> Cordiant
-            </label>
-            <a href="https://www.example.com">Показать все</a>
-            <span>(12)</span>
-         </div>
-         <div>
-            <p>Скидки</p>
-            <label>
-               <input type="checkbox" name="option1" /> Да
-            </label>
-            <p>
-               Runflat <span className={classes.tieresSpan}>?</span>
-            </p>
-            <label>
-               <input type="checkbox" name="option1" /> Да
-            </label>
-            <p>
-               Off-Road <span className={classes.tieresSpan}>?</span>
-            </p>
-            <label>
-               <input type="checkbox" name="option1" /> Да
-            </label>
-         </div>
-         <div>
+      <form>
+         <div className={classes.tieres}>
+            <h1>Шины в Бишкеке</h1>
             <p className={classes.tieresTitle}>
-               Индекс скорости <span className={classes.tieresSpan}>?</span>
+               Ширина <span className={classes.tieresSpan}>?</span>
             </p>
-            <select name="H(934)" id="">
-               <option value="Все">H (934)</option>
+            <select name="Все" id="">
+               <option value="Все">Все</option>
                <option value="Все">Все</option>
                <option value="Все">Все</option>
             </select>
-         </div>
-         <div className={classes.price}>
-            <div className={classes.priceitemInput}>
-               <input type="text" placeholder="от 1234" />
-               <input type="text" placeholder="до 50234" />
+            <p className={classes.tieresTitle}>
+               Профиль <span className={classes.tieresSpan}>?</span>
+            </p>
+            <select name="Все" id="">
+               <option value="Все">Все</option>
+               <option value="Все">Все</option>
+               <option value="Все">Все</option>
+            </select>
+            <p className={classes.tieresTitle}>
+               Диаметр <span className={classes.tieresSpan}>?</span>
+            </p>
+            <select name="Все" id="">
+               <option value="Все">Все</option>
+               <option value="Все">Все</option>
+               <option value="Все">Все</option>
+            </select>
+            <p className={classes.tieresTitle}>Цена</p>
+            <div className={classes.price}>
+               <div className={classes.priceitemInput}>
+                  <input type="text" placeholder="от 1234" />
+                  <input type="text" placeholder="до 50234" />
+               </div>
+               <input type="range" name="" id="" />
             </div>
-            <input type="range" />
-         </div>
-         <div>
-            <p>Топливная экономичность</p>
-            <label>
-               <input type="checkbox" name="option1" /> A
-            </label>
-            <label>
-               <input type="checkbox" name="option1" /> B
-            </label>
-            <label>
-               <input type="checkbox" name="option1" /> C
-            </label>
-            <label>
-               <input type="checkbox" name="option1" /> D
-            </label>
-            <label>
-               <input type="checkbox" name="option1" /> E
-            </label>
-            <label>
-               <input type="checkbox" name="option1" /> F
-            </label>
-            <label>
-               <input type="checkbox" name="option1" /> G
-            </label>
-            <p>Сцепление с мокрой поверхностью</p>
-            <label>
-               <input type="checkbox" name="option1" /> A
-            </label>
-            <label>
-               <input type="checkbox" name="option1" /> B
-            </label>
-            <label>
-               <input type="checkbox" name="option1" /> C
-            </label>
-            <label>
-               <input type="checkbox" name="option1" /> D
-            </label>
-            <label>
-               <input type="checkbox" name="option1" /> E
-            </label>
-            <label>
-               <input type="checkbox" name="option1" /> F
-            </label>
-         </div>
-         <div className={classes.price}>
-            <p>Уровень внешнего шума</p>
-            <div className={classes.priceitemInput}>
-               <input type="text" placeholder="от 1234" />
-               <input type="text" placeholder="до 50234" />
+            <div className={classes.tireType}>
+               <p>Тип шин</p>
+               <label>
+                  <input type="checkbox" name="option1" /> Легковое
+               </label>
+
+               <label>
+                  <input type="checkbox" name="option2" />
+                  Внедорожник (SUV)
+               </label>
+
+               <label>
+                  <input type="checkbox" name="option3" /> Легкогрузовой (LTR)
+               </label>
+               <p>Сезонность</p>
+               <label>
+                  <input type="checkbox" name="option1" /> Зима
+               </label>
+
+               <label>
+                  <input type="checkbox" name="option2" /> Лето
+               </label>
+
+               <label>
+                  <input type="checkbox" name="option3" /> Всесезонное
+               </label>
+               <p>Состояние</p>
+               <label>
+                  <input type="checkbox" name="option1" /> Только новые
+               </label>
             </div>
-            <input type="range" />
-         </div>
-         <div>
-            <button>Подобрать</button>
-            <button>Сбросить все</button>
+            <div className={classes.inputContainer}>
+               <p>Производитель</p>
+               <span className={classes.icon}>🔍</span>
+               <input
+                  type="text"
+                  className={classes.inputText}
+                  placeholder="Введите название"
+               />
+            </div>
+            <div>
+               <label>
+                  <input type="checkbox" name="option1" /> Nokian
+               </label>
+               <label>
+                  <input type="checkbox" name="option1" /> Pirelli
+               </label>
+               <label>
+                  <input type="checkbox" name="option1" /> Hankook
+               </label>
+               <label>
+                  <input type="checkbox" name="option1" /> Michelin
+               </label>
+               <label>
+                  <input type="checkbox" name="option1" /> Cordiant
+               </label>
+               <a href="https://www.example.com">Показать все</a>
+               <span>(12)</span>
+            </div>
+            <div>
+               <p>Скидки</p>
+               <label>
+                  <input type="checkbox" name="option1" /> Да
+               </label>
+               <p>
+                  Runflat <span className={classes.tieresSpan}>?</span>
+               </p>
+               <label>
+                  <input type="checkbox" name="option1" /> Да
+               </label>
+               <p>
+                  Off-Road <span className={classes.tieresSpan}>?</span>
+               </p>
+               <label>
+                  <input type="checkbox" name="option1" /> Да
+               </label>
+            </div>
+            <div>
+               <p className={classes.tieresTitle}>
+                  Индекс скорости <span className={classes.tieresSpan}>?</span>
+               </p>
+               <select name="H(934)" id="">
+                  <option value="Все">H (934)</option>
+                  <option value="Все">Все</option>
+                  <option value="Все">Все</option>
+               </select>
+            </div>
+            <div className={classes.price}>
+               <div className={classes.priceitemInput}>
+                  <input type="text" placeholder="от 1234" />
+                  <input type="text" placeholder="до 50234" />
+               </div>
+               <input type="range" />
+            </div>
+            <div>
+               <p>Топливная экономичность</p>
+               <label>
+                  <input type="checkbox" name="option1" /> A
+               </label>
+               <label>
+                  <input type="checkbox" name="option1" /> B
+               </label>
+               <label>
+                  <input type="checkbox" name="option1" /> C
+               </label>
+               <label>
+                  <input type="checkbox" name="option1" /> D
+               </label>
+               <label>
+                  <input type="checkbox" name="option1" /> E
+               </label>
+               <label>
+                  <input type="checkbox" name="option1" /> F
+               </label>
+               <label>
+                  <input type="checkbox" name="option1" /> G
+               </label>
+               <p>Сцепление с мокрой поверхностью</p>
+               <label>
+                  <input type="checkbox" name="option1" /> A
+               </label>
+               <label>
+                  <input type="checkbox" name="option1" /> B
+               </label>
+               <label>
+                  <input type="checkbox" name="option1" /> C
+               </label>
+               <label>
+                  <input type="checkbox" name="option1" /> D
+               </label>
+               <label>
+                  <input type="checkbox" name="option1" /> E
+               </label>
+               <label>
+                  <input type="checkbox" name="option1" /> F
+               </label>
+            </div>
+            <div className={classes.price}>
+               <p>Уровень внешнего шума</p>
+               <div className={classes.priceitemInput}>
+                  <input type="text" placeholder="от 1234" />
+                  <input type="text" placeholder="до 50234" />
+               </div>
+               <input type="range" />
+            </div>
+            <div>
+               <button>Подобрать</button>
+               <button>Сбросить все</button>
+            </div>
          </div>
       </form>
    )
