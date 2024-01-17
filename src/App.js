00b5566e57e30ce6c466f7f01a0a662180428f48
Azis <you@example.com>
2024-01-17 13:17:14 +0600

    feat: new folder tire-information

--- src/App.js
+++ src/App.js
@@ -1,14 +1,14 @@
 import React from 'react'
 import Layout from './layout/Layout'
-// import HomePage from "./components/home/HomePage"
-import CatalogPage from './components/catalog/CatalogPage'
-// import CardPage from './components/card-product/CardPage'
+// import HomePage from './components/home/HomePage'
+// import CatalogPage from './components/catalog/CatalogPage'
+import CardPage from './components/card-product/CardPage'
 
 const App = () => (
    <Layout>
       {/* <HomePage /> */}
-      <CatalogPage />
-      {/* <CardPage /> */}
+      {/* <CatalogPage /> */}
+      <CardPage />
    </Layout>
 )
 
--- src/components/card-product/CardPage.jsx
+++ src/components/card-product/CardPage.jsx
@@ -5,6 +5,7 @@ import CardDescription from './description/CardDescription'
 const CardPage = () => {
    return (
       <div className={classes.container}>
+         <h1>Шины летние Cordiant Sport 3 PS2 215/65 R16 102V</h1>
          <CardDescription />
       </div>
    )
