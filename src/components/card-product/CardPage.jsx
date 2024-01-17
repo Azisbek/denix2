df917fdef2359f77536d5104acd8d969294de9e5
syrttan1 <syrttansyrgakuulu.pi.1.20@gmail.com>
2024-01-17 17:38:00 +0600

    feat: create tabs

--- src/components/card-product/CardPage.jsx
+++ src/components/card-product/CardPage.jsx
@@ -7,7 +7,6 @@ import SimivarProducts from './simivar-products/SimivarProducts'
 const CardPage = () => {
    return (
       <div className={classes.container}>
-         <h1>Шины летние Cordiant Sport 3 PS2 215/65 R16 102V</h1>
          <PathBasket />
          <CardDescription />
          <SimivarProducts />
