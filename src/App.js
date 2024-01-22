7fadc644a258d0277a2b040d28b70978e6ee41f7
Azis <you@example.com>
2024-01-22 16:19:06 +0600

    fix: error

--- src/App.js
+++ src/App.js
@@ -4,24 +4,24 @@ import Layout from './layout/Layout'
 // import FavoritesPage from './components/favorites/FavoritesPage'
 // import SignIn from './components/form/SignIn'
 // import SignUp from './components/form/SignUp'
-import CartPage from './components/cart/CartPage'
+// import CartPage from './components/cart/CartPage'
 // import FavoritesPage from './components/favorites/FavoritesPage'
 // import HomePage from './components/home/HomePage'
 // import CatalogPage from './components/catalog/CatalogPage'
-// import CardPage from './components/card-product/CardPage'
+import CardPage from './components/card-product/CardPage'
 // import Contacts from './components/contacts/Сontacts'
 
 const App = () => (
    <Layout>
       {/* <HomePage /> */}
       {/* <CatalogPage /> */}
-      {/* <CardPage /> */}
+      <CardPage />
       {/* <FavoritesPage /> */}
       {/* <SignIn /> */}
       {/* <SignUp /> */}
       {/* <AboutCompanyPage /> */}
       {/* <Contacts /> */}
-      <CartPage />
+      {/* <CartPage />  */}
    </Layout>
 )
 
