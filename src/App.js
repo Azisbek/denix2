3664598442442771362e02cbbff58ec13dac0de2
syrttan1 <syrttansyrgakuulu.pi.1.20@gmail.com>
2024-01-23 16:48:51 +0600

    feat: merge cart

--- src/App.js
+++ src/App.js
@@ -1,14 +1,16 @@
 import React from 'react'
 import Layout from './layout/Layout'
+import OrderPage from './components/order-page/OrderPage'
 // import HomePage from './components/home/HomePage'
 // import CatalogPage from './components/catalog/CatalogPage'
-import CardPage from './components/card-product/CardPage'
+// import CardPage from './components/card-product/CardPage'
 
 const App = () => (
    <Layout>
       {/* <HomePage /> */}
       {/* <CatalogPage /> */}
-      <CardPage />
+      {/* <CardPage /> */}
+      <OrderPage />
    </Layout>
 )
 
