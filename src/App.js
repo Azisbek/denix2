d7c165498ed7ddd6470039f898350741fc187922
srm98765 <samaraedilkyzy94@gmail.com>
2024-01-31 18:49:25 +0600

    feat: complete news-components

--- src/App.js
+++ src/App.js
@@ -1,6 +1,8 @@
 import React from 'react'
 import Layout from './layout/Layout'
-import PersonalAccount from './components/personal-area/PersonalAccount'
+import NewsArticles from './components/news-articles/NewsArticles'
+// import NewsAndArticles from './components/news-file/NewsAndArticles'
+// import PersonalAccount from './components/personal-area/PersonalAccount'
 // import FavoritesPage from './components/favorites/FavoritesPage'
 // import HomePage from './components/home/HomePage'
 // import CatalogPage from './components/catalog/CatalogPage'
@@ -8,7 +10,9 @@ import PersonalAccount from './components/personal-area/PersonalAccount'
 
 const App = () => (
    <Layout>
-      <PersonalAccount />
+      <NewsArticles />
+      {/* <NewsAndArticles /> */}
+      {/* <PersonalAccount /> */}
       {/* <HomePage /> */}
       {/* <CatalogPage /> */}
       {/* <CardPage /> */}
